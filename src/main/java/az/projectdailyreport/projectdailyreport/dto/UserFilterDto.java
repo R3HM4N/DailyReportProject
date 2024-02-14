e257fab771182c29215a5c4e14575ac7815628af
Rehman Eliyev <125017696+R3HM4N@users.noreply.github.com>
2024-02-14 12:27:41 +0400

    Update UserFilterDto.java

--- src/main/java/az/projectdailyreport/projectdailyreport/dto/UserFilterDto.java
+++ src/main/java/az/projectdailyreport/projectdailyreport/dto/UserFilterDto.java
@@ -1,15 +1,13 @@
 package az.projectdailyreport.projectdailyreport.dto;
 
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Data
 @RequiredArgsConstructor
-public class UserFiltirDto {
+public class UserFilterDto {
     private Long id;
     private String firstName;
     private String lastName;
